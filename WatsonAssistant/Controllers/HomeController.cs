bb3f65051f8c2aa02e1522e5c5b8b1048670f14a
Mahmood Aljuboori <moe_sh24@yahoo.com>
2018-11-10 22:26:17 -0500

    Base version of Watson Assistant chatbot

---
+++ WatsonAssistant/Controllers/HomeController.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using IBM.WatsonDeveloperCloud.Assistant.v1;
+using IBM.WatsonDeveloperCloud.Assistant.v1.Model;
+using WatsonAssistant.Models;
+using IBM.WatsonDeveloperCloud.Util;
+
+namespace WatsonAssistant.Controllers
+{
+    public class HomeController : Controller
+    {
+        private AssistantService _assistant;
+        private MessageResponse response;
+        private string v1VersionDate = "2018-02-16";
+
+        public IActionResult Index()
+        {
+            _assistant = new AssistantService();
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public string Message(string message)
+        {
+
+            TokenOptions iamAssistantTokenOptions = new TokenOptions()
+            {
+                IamApiKey = "api key",
+                ServiceUrl = "https://gateway-wdc.watsonplatform.net/assistant/api"
+            };
+
+            _assistant = new AssistantService(iamAssistantTokenOptions, "2018-02-16");
+            response = new MessageResponse();
+
+            //  create message request
+            MessageRequest messageRequest0 = new MessageRequest()
+            {
+                Input = new InputData()
+                {
+                    Text = message
+                },
+                Context = HttpContext.Session.Get("Context")
+            };
+
+            //  send a message to the assistant instance
+            response = _assistant.Message("workspace id", messageRequest0);
+
+            return response.Output["text"][0].ToString();
+        }
+    }
+}
---
+++ WatsonAssistant/wwwroot/js/site.js
@@ -0,0 +1,23 @@
+﻿// Write your JavaScript code.
+function updateScroll() {
+    var element = document.getElementById("messagesDiv");
+    element.scrollTop = element.scrollHeight;
+}
+
+function CallAssistant() {
+
+    var message = $("#inputMessage").val();
+    $("#messagesDiv").append("<div class='green_box'><span>" + message + "</span></div>");
+    $("#inputMessage").val("");
+    $.ajax({
+        url: "/Home/CallAssistant",
+        type: "POST",
+        data: "{ Message: '" + message + "' }",
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        complete: function (response) {
+            $("#messagesDiv").append("<div class='blue_box'><span>" + response.responseText + "</span></div>");
+            updateScroll();
+        }
+    })
+}
